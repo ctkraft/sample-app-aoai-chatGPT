275424b0f03fa6d2cf60b33d067d52853773e6b5
Chen <andre.chen@afs.com>
2024-01-24 14:33:05 -0500

    updated prompt used in experimentation to align with updates made in app

--- notebooks/experiment_engine/test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-if __name__ == "__main__": 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--test", type=str)
-    args = parser.parse_args()
-    print(args.test)
